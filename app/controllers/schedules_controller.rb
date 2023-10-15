bfd69d813f09a293da3488a282f354de0db5bf22
Nanashi1122 <os1122skyl@gmail.com>
2023-10-15 17:13:31 +0900

    アプリのトップページと新規追加ページの実装

---
+++ app/controllers/schedules_controller.rb
@@ -0,0 +1,21 @@
+class SchedulesController < ApplicationController
+    def index
+        @schedules = Schedule.all
+    end
+    
+    def new
+        @schedule = Schedule.new
+    end
+
+    def create
+    end
+    
+    def show
+    end
+
+    def edit
+    end
+
+    def destroy
+    end
+end
