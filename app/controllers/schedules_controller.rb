8caf6d1a1a64ee14bd41d496d16a49a8cd51ecdb
Nanashi1122 <os1122skyl@gmail.com>
2023-10-15 22:12:25 +0900

    参照ページの実装

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -8,9 +8,17 @@ class SchedulesController < ApplicationController
     end
 
     def create
+        @schedule = Schedule.new(params.require(:schedule).permit(:title, :start_day, :end_day, :all_day))
+        if @schedule.save
+            flash[:notice] = "スケジュールを新規登録しました"
+            redirect_to :schedules
+        else
+            render "new"
+        end
     end
     
     def show
+        @schedule = Schedule.find(params[:id])
     end
 
     def edit
