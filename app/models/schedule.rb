e8c299c6060c369ec8b111279fc23a38df835b77
Nanashi1122 <os1122skyl@gmail.com>
2023-10-21 18:50:26 +0900

    新規追加にバリデーションの適用

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,5 @@
 class Schedule < ApplicationRecord
+    validates :title, presence: true, length: {maximum: 500}
+    validates :start_day, presence: true
+    validates :end_day, presence: true
 end
